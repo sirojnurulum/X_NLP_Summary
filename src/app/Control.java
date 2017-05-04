f93578dcc48264ad73f2596fc306c3a778715fc6
sirojnurulum <sirojnurulum@gmail.com>
2017-05-04 14:07:58 +0700

    new

--- src/app/Control.java
+++ src/app/Control.java
@@ -7,6 +7,9 @@ package app;
 
 import IndonesianNLP.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -33,17 +36,73 @@ public class Control {
         this.is = new IndonesianStemmer();
     }
 
+    public String createSummary(ArrayList<String> docs, HashMap<String, Double> tfidf) {
+        String hasil = "";
+        ArrayList<String> tmp = new ArrayList<>();
+        for (Map.Entry<String, Double> entry : tfidf.entrySet()) {
+            tmp.add(entry.getKey());
+        }
+        System.out.println(Arrays.toString(tmp.toArray()));
+        for (int i = 0; i < tmp.size() - 1; i++) {
+            for (int j = i + 1; j < tmp.size(); j++) {
+                if (tfidf.get(tmp.get(i)) < tfidf.get(tmp.get(j))) {
+                    String x = tmp.get(i);
+                    tmp.set(i, tmp.get(j));
+                    tmp.set(j, x);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(tmp.toArray()));
+        ArrayList<String> newDocs = new ArrayList<>();
+        for (int i = 0; i < docs.size(); i++) {
+            if (docs.get(i).contains(".")) {
+                String[] q = docs.get(i).split(".");
+                for (int j = 0; j < q.length; j++) {
+                    tmp.add(q[j]);
+                }
+            } else {
+                tmp.add(docs.get(i));
+            }
+        }
+        for (int i = 0; i < newDocs.size(); i++) {
+            if (newDocs.get(i).contains(tmp.get(0)) | newDocs.get(i).contains(tmp.get(1)) | newDocs.get(i).contains(tmp.get(2)) | newDocs.get(i).contains(tmp.get(3)) | newDocs.get(i).contains(tmp.get(4))) {
+                hasil = hasil + newDocs.get(i);
+                System.out.println(hasil);
+            }
+        }
+        return hasil;
+    }
+
     public double calculateTf(ArrayList<String> doc, String x) {
-        int hasil = 0;
-        hasil = doc.stream().filter((string) -> (string.equalsIgnoreCase(x))).map((_item) -> 1).reduce(hasil, Integer::sum);
-        return hasil / doc.size();
+        float hasil = 0;
+        for (int i = 0; i < doc.size(); i++) {
+            if (doc.get(i).equalsIgnoreCase(x)) {
+                hasil++;
+            }
+        }
+//        System.out.println("+++++++++++++++++++++++");
+//        System.out.println(x);
+//        System.out.println("count = " + hasil);
+//        System.out.println("div = " + hasil / doc.size());
+        return hasil / (float) doc.size();
     }
 
     public double calculateIdf(ArrayList<ArrayList<String>> docs, String x) {
-        int hasil = 0;
-        for (ArrayList<String> doc : docs) {
-            hasil = doc.stream().filter((string) -> (string.equalsIgnoreCase(x))).map((_item) -> 1).reduce(hasil, Integer::sum);
+        float hasil = 0;
+        for (int i = 0; i < docs.size(); i++) {
+            for (int j = 0; j < docs.get(i).size(); j++) {
+                if (x.equalsIgnoreCase(docs.get(i).get(j))) {
+                    hasil++;
+                    break;
+                }
+
+            }
         }
+//        System.out.println("+++++++++++++++++++++++++++++++++");
+//        System.out.println(x);
+//        System.out.println("docs size = " + docs.size());
+//        System.out.println("count = " + hasil);
+//        System.out.println("IDF = " + Math.log(docs.size() / hasil));
         return Math.log(docs.size() / hasil);
     }
 
@@ -52,18 +111,52 @@ public class Control {
     }
 
     public String doStopWord(String kalimat) {
-        System.out.println("++");
-        System.out.println(kalimat);
-        System.out.println("++ formalized sentence");
+//        System.out.println("++");
+//        System.out.println(kalimat);
+//        System.out.println("++ formalized sentence");
         kalimat = isf.formalizeSentence(kalimat);
-        System.out.println(kalimat);
-        System.out.println("++ formalized word");
-        System.out.println(isf.formalizeWord(kalimat));
-        System.out.println("++ stopword removed");
+//        System.out.println(kalimat);
+//        System.out.println("++ formalized word");
+//        System.out.println(isf.formalizeWord(kalimat));
+//        System.out.println("++ stopword removed");
         isf.initStopword();
         kalimat = isf.deleteStopword(kalimat);
-        System.out.println(kalimat);
+//        System.out.println(kalimat);
         return kalimat;
     }
-    
+
+    public String doStem(String kalimat) {
+        ArrayList<String> tmp = ist.tokenizeSentence(kalimat);
+        kalimat = "";
+        for (int i = 0; i < tmp.size(); i++) {
+            kalimat = kalimat + " " + is.stem(tmp.get(i));
+        }
+        return kalimat;
+    }
+
+    public ArrayList<String> doTokenisasi(String kalimat) {
+        return ist.tokenizeSentence(kalimat);
+    }
+
+    public void postag(ArrayList<String> docs) {
+        for (String[] string : IndonesianPOSTagger.doPOSTag(docs.get(0))) {
+            System.out.println("====postag");
+            for (String string1 : string) {
+                System.out.println(string1);
+            }
+        }
+    }
+
+    public void sentenceDetector(ArrayList<String> docs) {
+        IndonesianSentenceDetector isd = new IndonesianSentenceDetector();
+        System.out.println(docs.get(0));
+        System.out.println("---------------------------------------------");
+        isd.splitSentence(docs.get(0)).stream().map((String string) -> {
+            System.out.println(string);
+            return string;
+        }).forEachOrdered((_item) -> {
+            System.out.println("++");
+        });
+    }
+
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -5,14 +5,12 @@
  */
 package app;
 
-import IndonesianNLP.IndonesianPOSTagger;
-import IndonesianNLP.IndonesianSentenceDetector;
-import IndonesianNLP.IndonesianSentenceTokenizer;
-import IndonesianNLP.IndonesianStemmer;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,77 +21,89 @@ import java.util.logging.Logger;
 public class Main {
 
     ArrayList<String> docs;
+    ArrayList<String> docsStopWord;
+    ArrayList<String> docsStem;
+    HashMap<String, HashMap<String, Double>> tfDatas;
+    HashMap<String, Double> idfData;
+    HashMap<String, Double> tfIdfData;
+    String text;
     Control control;
 
     public Main() {
         docs = new ArrayList<>();
+        docsStopWord = new ArrayList<>();
+        docsStem = new ArrayList<>();
+        tfDatas = new HashMap<>();
+        idfData = new HashMap<>();
+        tfIdfData = new HashMap<>();
         control = new Control();
     }
 
     public void mulai() {
         readDataFromFile();
-//        sentenceDetector();
-//        tokenisasi();
-//        postag();
-//        stem();
-        control.doStopWord(docs.get(0));
+        removeStopWordAndStem();
+        getTf();
+        getIdf();
+//        System.out.println(tfIdfData.size());
+//        System.out.println("####################################################");
+//        for (int i = 0; i < docs.size(); i++) {
+//            System.out.println(docs.get(i));
+//        }
+//        System.out.println("####################################################");
+//        System.out.println(control.createSummary(docs, tfIdfData));
     }
 
-    public void stem() {
-        IndonesianStemmer is = new IndonesianStemmer();
-        ArrayList<String> tmp = new IndonesianSentenceTokenizer().tokenizeSentence(docs.get(0));
-        for (String string : tmp) {
-            System.out.println("---------");
-            System.out.println("kata : " + string);
-            System.out.println("Basic word : " + is.stem(string));
-            for (int i = 0; i < is.derivationalprefix.size(); i++) {
-                System.out.println("Derivational Prefix : " + is.derivationalprefix.get(i));
+    public void getTf() {
+        HashMap<String, Double> tfData;
+        for (int i = 0; i < docsStopWord.size(); i++) {
+            tfData = new HashMap<>();
+            ArrayList<String> tmp = control.doTokenisasi(docsStopWord.get(i));
+//            System.out.println("------------------------------------------");
+//            System.out.println("i index = " + i);
+//            System.out.println(docs.get(i));
+            for (int j = 0; j < tmp.size(); j++) {
+                if (tfData.get(tmp.get(j)) == null) {
+                    tfData.put(tmp.get(j), control.calculateTf(tmp, tmp.get(j)));
+//                    System.out.println(tmp.get(j) + " -> " + tfData.get(tmp.get(j)));
+                }
             }
-            System.out.println("Particle Suffix: " + is.particlesuffix);
-            System.out.println("Possessive Pronoun Suffix: " + is.possessivepronounsuffix);
-            System.out.println("Derivational Suffix: " + is.derivationalsuffix);
-
+//            System.out.println("------------------------------------------");
+            tfDatas.put(docs.get(i), tfData);
         }
     }
 
-    public void postag() {
-        for (String[] string : IndonesianPOSTagger.doPOSTag(docs.get(0))) {
-            System.out.println("====postag");
-            for (String string1 : string) {
-                System.out.println(string1);
+    public void getIdf() {
+        ArrayList<ArrayList<String>> tmp = new ArrayList<>();
+        for (int i = 0; i < docs.size(); i++) {
+            tmp.add(control.doTokenisasi(docs.get(i)));
+        }
+        for (Map.Entry<String, HashMap<String, Double>> entry : tfDatas.entrySet()) {
+            String key = entry.getKey();
+            HashMap<String, Double> tfData = entry.getValue();
+            for (Map.Entry<String, Double> entry1 : tfData.entrySet()) {
+                if (!idfData.containsKey(entry1.getKey())) {
+                    idfData.put(entry1.getKey(), control.calculateIdf(tmp, entry1.getKey()));
+                    System.out.println("" + entry1.getKey() + " : " + idfData.get(entry1.getKey()));
+                    //calculate tfidf
+                    tfIdfData.put(entry1.getKey(), (entry1.getValue() * idfData.get(entry1.getKey())));
+                    System.out.println("tfidf : " + entry1.getKey() + " = " + tfIdfData.get(entry1.getKey()));
+                }
             }
         }
     }
 
-    public void tokenisasi() {
-        IndonesianSentenceTokenizer ist = new IndonesianSentenceTokenizer();
-        System.out.println(docs.get(0));
-        System.out.println("--------------------------------------------");
-        ist.tokenizeSentence(docs.get(0)).stream().map((tek) -> {
-            System.out.println(tek);
-            return tek;
-        }).forEachOrdered((_item) -> {
-            System.out.println("++");
-        });
-        System.out.println("=============================================");
-        ist.tokenizeSentenceWithCompositeWords(docs.get(0)).stream().map((object) -> {
-            System.out.println(object);
-            return object;
-        }).forEachOrdered((_item) -> {
-            System.out.println("++");
-        });
-    }
-
-    public void sentenceDetector() {
-        IndonesianSentenceDetector isd = new IndonesianSentenceDetector();
-        System.out.println(docs.get(0));
-        System.out.println("---------------------------------------------");
-        isd.splitSentence(docs.get(0)).stream().map((String string) -> {
-            System.out.println(string);
-            return string;
-        }).forEachOrdered((_item) -> {
-            System.out.println("++");
-        });
+    public void removeStopWordAndStem() {
+        for (int i = 0; i < docs.size(); i++) {
+//            System.out.println("-->>docs");
+//            System.out.println(docs.get(i));
+            docsStopWord.add(control.doStopWord(docs.get(i)));
+//            System.out.println("docsStopWord : ");
+//            System.out.println(docsStopWord.get(i));
+            docsStem.add(control.doStem(docsStopWord.get(i)));
+//            System.out.println("docsStem : ");
+//            System.out.println(docsStem.get(i));
+//            System.out.println("\n");
+        }
     }
 
     public void readDataFromFile() {
@@ -102,6 +112,9 @@ public class Main {
             Object[] o = bf.lines().toArray();
             for (Object object : o) {
                 docs.add(String.valueOf(object));
+                text = text + String.valueOf(object) + "\n";
+//                System.out.println(String.valueOf(object));
+//                System.out.println("\n\n");
             }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
